1b9a70a0d8413fc9cae207f196f4f4136613282b
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-05-21 17:02:15 -0400

    Add logger class

--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -1,7 +1,9 @@
 #include "lib/aileron.hpp"
 #include "lib/elevator.hpp"
 #include "lib/rudder.hpp"
+#include "lib/logger.hpp"
 
+mcu::lib::logger logger;
 mcu::lib::aileron aileron;
 mcu::lib::elevator elevator;
 mcu::lib::rudder rudder;
@@ -9,8 +11,7 @@ mcu::lib::rudder rudder;
 mcu::lib::interface::controller *controllers[3];
 
 void setup() {
-  Serial.begin(115200);
-
+  logger.init();
   delay(2000);
 
   controllers[0] = &aileron;
@@ -24,9 +25,12 @@ void setup() {
   for (auto ctr : controllers) {
     ctr->test();
   }
+
+  delay(2000);
 }
 
 void loop() {
+  logger.log("******************");
   for (auto ctr : controllers) {
     ctr->step();
   }
