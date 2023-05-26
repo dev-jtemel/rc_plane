cad9368d64c4422631082bbffa1a0c38338b8dc9
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-05-25 20:27:02 -0400

    Add top level build script

---
+++ full_build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ROOT_DIR="$PWD"
+DEV="/dev/ttyACM0"
+
+echo "*** COMPILING ***"
+bash scripts/compile-mcu.sh "$ROOT_DIR"
+bash scripts/compile-pc.sh "$ROOT_DIR"
+
+echo "*** FLASHING ***"
+bash scripts/flash-mcu.sh "$ROOT_DIR" "$DEV"
