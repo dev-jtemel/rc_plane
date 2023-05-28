c0e50b3aa2d685548b6bfc607138c63b25bdd0d8
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-05-28 09:16:57 -0400

    Update build script

--- full_build.sh
+++ full_build.sh
@@ -6,6 +6,7 @@ DEV="/dev/ttyACM0"
 echo "*** COMPILING ***"
 bash scripts/compile-mcu.sh "$ROOT_DIR"
 bash scripts/compile-pc.sh "$ROOT_DIR"
+bash scripts/compile-som.sh "$ROOT_DIR"
 
 echo "*** FLASHING ***"
 bash scripts/flash-mcu.sh "$ROOT_DIR" "$DEV"
