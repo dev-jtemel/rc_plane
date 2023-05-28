883ecda1559801a25a00c7b3b7becaba89c65a07
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-05-28 08:51:54 -0400

    Less bloat on MCU

--- mcu/lib/interface/switch_controller.hpp
+++ mcu/lib/interface/switch_controller.hpp
@@ -12,9 +12,6 @@ class switch_controller {
  public:
   switch_controller() = default;
 
-  switch_controller(String TAG_) : TAG(TAG_) {
-  }
-
   ~switch_controller() = default;
 
   virtual bool setup() {
@@ -25,43 +22,8 @@ class switch_controller {
     return false;
   }
 
-  template <typename T>
-  void log(T t) {
-    if (_log_open) {
-      Serial.print(t);
-      return;
-    }
-
-    Serial.print("[");
-    Serial.print(micros());
-    Serial.print("] ");
-    Serial.print("[");
-    Serial.print(TAG);
-    Serial.print("] ");
-    Serial.println(t);
-  }
-
-  void open_log() {
-    Serial.print("[");
-    Serial.print(micros());
-    Serial.print("] ");
-    Serial.print("[");
-    Serial.print(TAG);
-    Serial.print("] ");
-    _log_open = true;
-  }
-
-  void close_log() {
-    Serial.println();
-    _log_open = false;
-  }
-
  protected:
   const double PVM_MID = 1500.00;
-  String TAG;
- 
- private:
-  bool _log_open;
 };
 
 } // namespace interface
--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -1,15 +1,75 @@
-#include "lib/utils/logger.hpp"
-#include "lib/mcu_manager.hpp"
+#include "./lib/controllers/aileron.hpp"
+#include "./lib/controllers/elevator.hpp"
+#include "./lib/controllers/rudder.hpp"
+#include "./lib/controllers/motor.hpp"
+#include "./lib/controllers/test_switch.hpp"
+#include "./lib/controllers/flight_switch.hpp"
+#include "./lib/flag.hpp"
 
-mcu::lib::logger logger;
-mcu::lib::mcu_manager mcu_manager;
+const uint8_t CONTROLLERS_COUNT = 4U;
+const String PADDING = "000000000000000000000000";
+uint8_t STATE = 0U;
+
+mcu::lib::motor motor;
+mcu::lib::aileron aileron;
+mcu::lib::elevator elevator;
+mcu::lib::rudder rudder;
+
+mcu::lib::interface::controller *controllers[CONTROLLERS_COUNT];
+
+mcu::lib::test_switch test_switch;
+mcu::lib::flight_switch flight_switch;
+
+void write_state() {
+  Serial.print(PADDING);
+  for (int i = CONTROLLERS_COUNT - 1; i >= 0; --i) {
+    controllers[i]->write_state();
+  }
+  for (int i = 7; i >= 0; --i) {
+    Serial.print(static_cast<bool>(bitRead(STATE, i)));
+  }
+  Serial.println();
+}
 
 void setup() {
-  logger.init();
-  mcu_manager.init();
-  mcu_manager.setup();
+  Serial.begin(115200);
+
+  test_switch.setup();
+  flight_switch.setup();
+
+  controllers[0] = &motor;
+  controllers[1] = &aileron;
+  controllers[2] = &elevator;
+  controllers[3] = &rudder;
+
+  for (auto ctr : controllers) {
+    ctr->setup();
+  }
+
+  delay(2000);
+
+  uint8_t i = 0;
+  write_state();
+  for (auto ctr : controllers) {
+    ctr->test();
+    STATE |= mcu::lib::flag::TEST_FLAGS[i++];
+    write_state();
+  }
+  delay(1000);
 }
 
 void loop() {
-  mcu_manager.step();
+  if (flight_switch.state()) {
+    STATE |= mcu::lib::flag::FLIGHT_MODE;
+    for (auto ctr : controllers) {
+      ctr->step();
+    }
+  } else {
+    STATE &= ~mcu::lib::flag::FLIGHT_MODE;
+    for (auto ctr : controllers) {
+      ctr->stop();
+    }
+    delay(500);
+  }
+  write_state();
 }
