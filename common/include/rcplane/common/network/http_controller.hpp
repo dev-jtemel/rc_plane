ae53ecf52e29c4d2ee2fd06a0bdb6e51234d46bd
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-08-11 15:34:46 -0400

    Add http server docs

--- common/include/rcplane/common/network/http_controller.hpp
+++ common/include/rcplane/common/network/http_controller.hpp
@@ -10,13 +10,37 @@ namespace rcplane {
 namespace common {
 namespace network {
 
+/**
+* @brief Control and host a HTTP server.
+*/
 class http_controller : public interface::network_interface {
 public:
+  /**
+   * @param termination_handler Hanlder to fire on termination.
+   */
   explicit http_controller(std::function<void(int)> termination_handler);
   ~http_controller();
 
+  /**
+   * @brief Initialize the http server.
+   *
+   * Handles registering paths and API to expose over https.
+   *
+   * @return Status of the initialization.
+   */
   bool init() override;
+
+  /**
+   * @brief Start the HTTP server.
+   * @pre init()
+   */
   void start() override;
+
+  /**
+   * @brief Terminate the HTTP server.
+   * @pre start()
+   * @warning Sends a CURL request /stop to internally destruct the underlying server.
+   */
   void terminate() override;
 
 private:
