f2668edfe49542d44d20a424c2cc96f390926311
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-08-11 15:25:44 -0400

    Packet docs

--- common/include/rcplane/common/io/packet.hpp
+++ common/include/rcplane/common/io/packet.hpp
@@ -7,6 +7,9 @@ namespace rcplane {
 namespace common {
 namespace io {
 
+/**
+ * @brief Available packet types supported.
+ */
 enum class packet_type : uint8_t {
   state = 0,
   motor = 1,
@@ -26,11 +29,17 @@ enum class packet_type : uint8_t {
   invalid = 255,
 };
 
+/**
+ * @brief Construct a packet to convert serial data into desired type.
+ */
 template<typename BUFFER_SIZE, typename RETURN_TYPE>
 class packet {
 public:
   packet() : packet(packet_type::invalid) {}
 
+  /**
+   * \warning RETURN_TYPE must be more or equal bytes to BUFFER_SIZE.
+   */
   explicit packet(packet_type type) {
     // Ensure that conversions will be perserved
     static_assert(sizeof(BUFFER_SIZE) <= sizeof(RETURN_TYPE));
@@ -39,14 +48,31 @@ public:
 
   ~packet() = default;
 
+  /**
+   * @brief Set the raw buffer data.
+   *
+   * @param buffer The buffer to set and later convert.
+   */
   void set(BUFFER_SIZE buffer) { _converter.raw = buffer; }
 
+  /**
+   * @return The raw buffer contents.
+   */
   BUFFER_SIZE raw() { return _converter.raw; }
 
+  /**
+   * @return The converted buffer contents.
+   */
   RETURN_TYPE data() { return _converter.data; }
 
+  /**
+   * @return The type of packet.
+   */
   packet_type type() { return _type; }
 
+  /**
+   * @brief Helper to convert the packet type to a string.
+   */
   std::string type_to_str() {
     switch (_type) {
       case packet_type::state: return "state";
@@ -65,9 +91,13 @@ public:
 private:
   packet_type _type;
 
-  // Trick to fast conversion. Set the buffer here and pull
-  // the data to get the value in it's expected return type
-  // since they share the same memory region.
+  /**
+   * @brief Conversion union.
+   *
+   * Trick to fast conversion. Set the buffer here and pull
+   * the data to get the value in it's expected return type
+   * since they share the same memory region.
+   */
   union conversion {
     BUFFER_SIZE raw;
     RETURN_TYPE data;
