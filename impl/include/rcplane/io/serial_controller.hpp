1aaf506add65f67335d0d457b4719e36f7040b93
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-08-17 21:20:44 -0400

    Link serial controller

--- impl/include/rcplane/io/serial_controller.hpp
+++ impl/include/rcplane/io/serial_controller.hpp
@@ -1,13 +1,14 @@
 #ifndef __RCPLANE__COMMON__IO__SERIAL_CONTROLLER_HPP__
 #define __RCPLANE__COMMON__IO__SERIAL_CONTROLLER_HPP__
 
-#include <array>
 #include <boost/asio.hpp>
 #include <boost/optional.hpp>
 #include <boost/optional/optional_io.hpp>
+#include <boost/thread.hpp>
 #include <fstream>
-#include <functional>
 #include <string>
+#include <thread>
+#include <chrono>
 
 #include "rcplane/base_controller.hpp"
 #include "rcplane/io/journal.hpp"
@@ -28,8 +29,19 @@ public:
    * @brief Construct a serial controller and hold the io_service reference.=
    * @param io The io_context of the main application.
    */
-  explicit serial_controller(boost::asio::io_service &io);
-  virtual ~serial_controller();
+  explicit serial_controller(boost::asio::io_context &io)
+  : interface::base_controller("serial-controller"), _io(io), _serial(_io) {
+    RCPLANE_ENTER();
+
+    TTY = config_manager::instance().get<std::string>(
+        "common.io.serial_controller.dev");
+    BAUDRATE = config_manager::instance().get<uint32_t>(
+        "common.io.serial_controller.baudrate");
+  }
+
+  virtual ~serial_controller() {
+    RCPLANE_ENTER();
+  }
 
   /**
    * @brief Connect to the serial port.
@@ -37,10 +49,19 @@ public:
    * Connect to the serial port with 115200 baudrate. Flush the serial
    * port on connection and perform a handshake with the microcontroller.
    *
-   * @warning Blocks until p_handshake_mcu() and p_flush() complete.
+   * @warning Blocks until handshake_mcu() and flush() complete.
    * @return Status of the initialization.
    */
   bool init() {
+    RCPLANE_ENTER();
+
+    static_assert(sizeof(float) == 4U);
+
+    if (!open_port()) { return false; }
+
+    if (!handshake_mcu()) { return false; }
+
+    RCPLANE_LOG(info, _tag, "initialized");
     return true;
   }
 
@@ -52,7 +73,10 @@ public:
    * @pre init() succeeded.
    */
   void start() {
-    
+    RCPLANE_ENTER();
+    _running = true;
+    _worker = boost::thread(&serial_controller::read_serial, this);
+    RCPLANE_LOG(info, _tag, "started");
   }
 
   /**
@@ -61,7 +85,8 @@ public:
    * @pre start() 
    */
   void terminate() {
-    
+    RCPLANE_ENTER();
+    _running = false;
   }
 
 private:
@@ -71,7 +96,12 @@ private:
    * Handle line switching and forwarding the correct data to the correct callbacks.
    */
   virtual void read_serial() {
+    RCPLANE_ENTER();
 
+    while (_running) {
+      auto read_buffer = read_line();
+      if (!read_buffer) { continue; }
+    }
   }
 
   /**
@@ -82,7 +112,19 @@ private:
    * @returns Status of openning the serial port.
    */
   virtual bool open_port() {
+    RCPLANE_ENTER();
 
+    try {
+      _serial.open(TTY);
+      _serial.set_option(boost::asio::serial_port_base::baud_rate(BAUDRATE));
+
+      ::tcflush(_serial.lowest_layer().native_handle(), TCIFLUSH);
+      flush();
+      return true;
+    } catch (...) {
+      RCPLANE_LOG(error, _tag, "failed to open serial port");
+      return false;
+    }
   }
 
   /**
@@ -94,7 +136,15 @@ private:
    * @throws boost::system::system_error on write failure.
    */
   virtual bool handshake_mcu() {
-    
+    RCPLANE_ENTER();
+
+    const auto k_write_delay = 1U;
+    for (uint8_t i = 0U; i < 2U; ++i) {
+      boost::asio::write(_serial,
+                        boost::asio::buffer(kHELLO_TX.c_str(), kHELLO_RX.size()));
+      std::this_thread::sleep_for(std::chrono::seconds(k_write_delay));
+    }
+    return true;
   }
 
   /**
@@ -104,28 +154,50 @@ private:
    *          if conversion to uint64_t failed.
    */
   virtual boost::optional<uint64_t> read_line() {
-    
+    RCPLANE_ENTER();
+
+    boost::asio::read_until(_serial, _streambuffer, '\n');
+
+    std::string res{};
+    std::istream is(&_streambuffer);
+    std::getline(is, res);
+
+    _blackbox << res << std::endl;
+    RCPLANE_LOG(info, _tag, res);
+
+    try {
+      return {std::stoul(res, nullptr, 16)};
+    } catch (...) { return {}; }
   }
 
   /**
    * @brief Flush the serial buffers.
    */
-  void flush();
+  void flush() {
+    RCPLANE_ENTER();
 
-  /**
-   * @brief Handle coversion of _buffer into the designated packets.
-   */
-  void handle_buffer();
+    std::string res{};
+    while (res != kHELLO_RX) {
+      boost::asio::read_until(_serial, _streambuffer, '\n');
+      std::istream is(&_streambuffer);
+      std::getline(is, res);
+    }
+    RCPLANE_LOG(info, _tag, "flushed");
+  }
 
   std::string TTY{};
   uint32_t BAUDRATE{};
+  const std::string kHELLO_RX{"rcplane\r"};
+  const std::string kHELLO_TX{"1"};
+  std::atomic<bool> _running{false};
   std::ofstream _blackbox;
 
   uint8_t _line = 0;
   boost::asio::streambuf _streambuffer;
   uint64_t _buffer;
 
-  boost::asio::io_service _io;
+  boost::thread _worker;
+  boost::asio::io_service &_io;
   boost::asio::serial_port _serial;
 };
 
--- impl/src/som/main.cpp
+++ impl/src/som/main.cpp
@@ -1,9 +1,11 @@
 #include <boost/asio/executor_work_guard.hpp>
 #include <boost/asio/io_service.hpp>
 #include <csignal>
+#include <memory>
 
 #include "rcplane/io/config_manager.hpp"
 #include "rcplane/io/journal.hpp"
+#include "rcplane/io/serial_controller.hpp"
 
 const std::string kTAG = "som-controller";
 boost::asio::io_service io;
@@ -22,7 +24,15 @@ int main() {
   signal(SIGINT, termination_handler);
   RCPLANE_LOG(info, kTAG, "termination handler set");
 
+  std::unique_ptr<rcplane::io::serial_controller> serial_controller 
+      = std::make_unique<rcplane::io::serial_controller>(io);
+
+  serial_controller->init();
+  serial_controller->start();
+
   io.run();
+
+  serial_controller->terminate();
 
   return 1;
 }
