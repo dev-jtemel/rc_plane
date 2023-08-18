d0c720bd72c15ae322a76e2a134d32dcfb8eab50
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-08-18 12:18:01 -0400

    Add documentation to packet.hpp

--- impl/include/rcplane/common/packet.hpp
+++ impl/include/rcplane/common/packet.hpp
@@ -8,6 +8,12 @@
 namespace rcplane {
 namespace common {
 
+/**
+ * @brief Packet definition of control surface values.
+ * 
+ * This packet defines the control surface data to be communicated
+ * to and from the mcu and som.
+*/
 struct __attribute__((packed)) control_surface_packet {
   uint32_t timestamp;
   uint8_t state;
@@ -18,6 +24,11 @@ struct __attribute__((packed)) control_surface_packet {
 };
 
 #ifdef ARDUINO_ARCH_AVR
+/**
+ * @brief Write the packet to UART.
+ * @tparam PACKET The packet struct to write to the serial console.
+ * @param packet The packet (lvalue) to write.
+*/
 template<typename PACKET>
 void write_packet(PACKET &packet) {
   Serial.write((byte *)&packet, sizeof(packet));
