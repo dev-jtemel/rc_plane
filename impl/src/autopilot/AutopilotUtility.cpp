c318e7ae065f15cd06b140ec54f6cf1d733be0f4
Jonathon Temelkovski <dev.jtemel@gmail.com>
2023-08-30 14:51:08 -0400

    Update test cases

--- impl/src/autopilot/AutopilotUtility.cpp
+++ impl/src/autopilot/AutopilotUtility.cpp
@@ -28,7 +28,7 @@ const io::ConfigManager &AutopilotUtility::getConfigManager() const {
 uint8_t AutopilotUtility::bindRcThrottle(const uint8_t &value) const {
   RCPLANE_LOG_METHOD();
 
-  return bindToRange<uint8_t>(value, 0, 100, -c_maxThrottle, c_maxThrottle);
+  return bindToRange<uint8_t>(value, 0, 100, 0, c_maxThrottle);
 }
 
 int8_t AutopilotUtility::bindRcAileronDeflection(const int8_t &value) const {
--- impl/test/unit/AutopilotManagerTest.cpp
+++ impl/test/unit/AutopilotManagerTest.cpp
@@ -3,8 +3,10 @@
 #include <gtest/gtest.h>
 #include <memory>
 
+#include "rcplane/autopilot/AutopilotUtility.hpp"
 #include "rcplane/common/Packet.hpp"
 #include "rcplane/common/State.hpp"
+#include "rcplane/io/ConfigManager.hpp"
 #include "rcplane/io/Journal.hpp"
 
 namespace rcplane {
@@ -12,84 +14,26 @@ namespace test {
 
 class AutopilotManagerFixture : public ::testing::Test {
 protected:
-  void SetUp() override {}
-  void TearDown() override { m_AutopilotManager.reset(); }
+  void SetUp() override {
+    const std::string kConfigFile = "configs/test/unit/generalConfig.json";
 
-  std::unique_ptr<autopilot::AutopilotManager> m_AutopilotManager =
-      std::make_unique<autopilot::AutopilotManager>();
+    ASSERT_TRUE(m_configManager.loadConfig(kConfigFile));
+
+    m_AutopilotUtility =
+        std::make_unique<autopilot::AutopilotUtility>(m_configManager);
+    m_AutopilotManager = std::make_unique<autopilot::AutopilotManager>(
+        *m_AutopilotUtility.get());
+  }
+
+  void TearDown() override {
+    m_AutopilotUtility.reset();
+    m_AutopilotManager.reset();
+  }
+
+  io::ConfigManager m_configManager;
+  std::unique_ptr<autopilot::AutopilotUtility> m_AutopilotUtility;
+  std::unique_ptr<autopilot::AutopilotManager> m_AutopilotManager;
 };
-
-TEST_F(AutopilotManagerFixture, zeroToZero) {
-  const common::ImuPacket kImuPacket;
-  const common::RcRxPacket kRcRxPacket;
-
-  const common::ControlSurfacePacket controlSurfacePacket =
-      m_AutopilotManager->trigger(kRcRxPacket, kImuPacket);
-
-  EXPECT_EQ(0, controlSurfacePacket.motorSpeed);
-  EXPECT_EQ(0, controlSurfacePacket.aileronDeflection);
-  EXPECT_EQ(0, controlSurfacePacket.elevatorDeflection);
-  EXPECT_EQ(0, controlSurfacePacket.rudderDeflection);
-}
-
-TEST_F(AutopilotManagerFixture, aileronBind) {
-  const common::ImuPacket kImuPacket;
-  common::RcRxPacket rcRxPacket;
-  rcRxPacket.aileronStickPosition = 100;
-
-  const common::ControlSurfacePacket controlSurfacePacket =
-      m_AutopilotManager->trigger(rcRxPacket, kImuPacket);
-
-  EXPECT_EQ(0, controlSurfacePacket.motorSpeed);
-  EXPECT_EQ(30, controlSurfacePacket.aileronDeflection);
-  EXPECT_EQ(0, controlSurfacePacket.elevatorDeflection);
-  EXPECT_EQ(0, controlSurfacePacket.rudderDeflection);
-}
-
-TEST_F(AutopilotManagerFixture, elevatorBind) {
-  const common::ImuPacket kImuPacket;
-  common::RcRxPacket rcRxPacket;
-  rcRxPacket.elevatorStickPosition = -100;
-
-  const common::ControlSurfacePacket controlSurfacePacket =
-      m_AutopilotManager->trigger(rcRxPacket, kImuPacket);
-
-  EXPECT_EQ(0, controlSurfacePacket.motorSpeed);
-  EXPECT_EQ(0, controlSurfacePacket.aileronDeflection);
-  EXPECT_EQ(-40, controlSurfacePacket.elevatorDeflection);
-  EXPECT_EQ(0, controlSurfacePacket.rudderDeflection);
-}
-
-TEST_F(AutopilotManagerFixture, rudderBind) {
-  const common::ImuPacket kImuPacket;
-  common::RcRxPacket rcRxPacket;
-  rcRxPacket.rudderStickPosition = 50;
-
-  const common::ControlSurfacePacket controlSurfacePacket =
-      m_AutopilotManager->trigger(rcRxPacket, kImuPacket);
-
-  EXPECT_EQ(0, controlSurfacePacket.motorSpeed);
-  EXPECT_EQ(0, controlSurfacePacket.aileronDeflection);
-  EXPECT_EQ(0, controlSurfacePacket.elevatorDeflection);
-  EXPECT_EQ(10, controlSurfacePacket.rudderDeflection);
-}
-
-TEST_F(AutopilotManagerFixture, allBind) {
-  const common::ImuPacket kImuPacket;
-  common::RcRxPacket rcRxPacket;
-  rcRxPacket.motorStickPosition = 100;
-  rcRxPacket.aileronStickPosition = -10;
-  rcRxPacket.elevatorStickPosition = 50;
-  rcRxPacket.rudderStickPosition = -25;
-
-  const common::ControlSurfacePacket controlSurfacePacket =
-      m_AutopilotManager->trigger(rcRxPacket, kImuPacket);
-
-  EXPECT_EQ(100, controlSurfacePacket.motorSpeed);
-  EXPECT_EQ(-3, controlSurfacePacket.aileronDeflection);
-  EXPECT_EQ(20, controlSurfacePacket.elevatorDeflection);
-  EXPECT_EQ(-5, controlSurfacePacket.rudderDeflection);
-}
 
 TEST_F(AutopilotManagerFixture, handleState_noTransition) {
   const common::ImuPacket kImuPacket;
